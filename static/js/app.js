97abfd3257780be7a05dc54224eda82370b2b0b1
Cemrecicek <cemrecicek0932@gmail.com>
2023-11-15 19:31:02 +0300

    düzenlemeler

--- static/js/app.js
+++ static/js/app.js
@@ -8,8 +8,8 @@ document.querySelector('#dark-mode-toggle').addEventListener('click', () => {
 });
 
 const start_screen = document.querySelector('#start-screen');
-const game_screen=document.querySelector('#game-screen');
-const pause_screen= document.querySelector('#pause-screen');
+const game_screen = document.querySelector('#game-screen');
+const pause_screen = document.querySelector('#pause-screen');
 const result_screen = document.querySelector('#result-screen');
 
 const cells = document.querySelectorAll('.main-grid-cell');
@@ -20,9 +20,9 @@ const name_input = document.querySelector('#input-name');
 const number_inputs = document.querySelectorAll('.number');
 
 
-const player_name=document.querySelector('#player-name');
-const game_level=document.querySelector('#game-level');
-const game_time= document.querySelector('#game-time');
+const player_name = document.querySelector('#player-name');
+const game_level = document.querySelector('#game-level');
+const game_time = document.querySelector('#game-time');
 
 const result_time = document.querySelector('#result-time');
 
@@ -31,8 +31,8 @@ let level = CONSTANT.LEVEL[level_index];
 
 let timer = null;
 
-let pause=false;
-let seconds =0;
+let pause = false;
+let seconds = 0;
 
 let su = undefined;
 let su_answer = undefined;
@@ -46,20 +46,20 @@ const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
 const initGameGrid = () => {
     let index = 0;
 
-    for(let i = 0 ; i<Math.pow(CONSTANT.GRID_SIZE,2);i++){
-        let row=Math.floor(i/CONSTANT.GRID_SIZE);
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE);
         let col = i % CONSTANT.GRID_SIZE;
-        if (row===2 || row ===5) cells[index].style.marginBottom = '10px ';
-        if(col===2 || col===5) cells[index].style.marginRight='10px';
+        if (row === 2 || row === 5) cells[index].style.marginBottom = '10px ';
+        if (col === 2 || col === 5) cells[index].style.marginRight = '10px';
 
         index++;
     }
 }
 
-const setPlayerName = (name) =>localStorage.setItem('player_name',name);
+const setPlayerName = (name) => localStorage.setItem('player_name', name);
 const getPlayerName = () => localStorage.getItem('player_name');
 
-const showTime=(seconds)=>new Date(seconds*1000).toISOString().substr(11,8);
+const showTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11, 8);
 
 const clearSudoku = () => {
     for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
@@ -84,9 +84,9 @@ const initSudoku = () => {
         let row = Math.floor(i / CONSTANT.GRID_SIZE);
         let col = i % CONSTANT.GRID_SIZE;
 
-        cells[i].setAttribute('data-value',su.question[row][col]);
+        cells[i].setAttribute('data-value', su.question[row][col]);
 
-        if (su.question[row][col] !==0){
+        if (su.question[row][col] !== 0) {
             cells[i].classList.add('filled');
             cells[i].innerHTML = su.question[row][col];
         }
@@ -111,50 +111,50 @@ const loadSudoku = () => {
         let row = Math.floor(i / CONSTANT.GRID_SIZE);
         let col = i % CONSTANT.GRID_SIZE;
 
-        cells[i].setAttribute('data-value',su_answer[row][col]);
+        cells[i].setAttribute('data-value', su_answer[row][col]);
         cells[i].innerHTML = su_answer[row][col] !== 0 ? su_answer[row][col] : '';
-        if (su.question[row][col] !==0){
+        if (su.question[row][col] !== 0) {
             cells[i].classList.add('filled');
         }
     }
 }
 
-const hoverBg = (index) => {    
+const hoverBg = (index) => {
     let row = Math.floor(index / CONSTANT.GRID_SIZE);
     let col = index % CONSTANT.GRID_SIZE;
 
     let box_start_row = row - row % 3;
     let box_start_col = col - col % 3;
 
-    for (let i = 0; i<CONSTANT.BOX_SIZE; i++){
-        for (let j = 0; j< CONSTANT.BOX_SIZE; j++){
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
             let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
             cell.classList.add('hover');
         }
     }
 
     let step = 9;
-    while (index - step >= 0 ){
+    while (index - step >= 0) {
         cells[index - step].classList.add('hover');
-        step +=9;
+        step += 9;
     }
 
     step = 9;
-    while (index + step < 81 ){
+    while (index + step < 81) {
         cells[index + step].classList.add('hover');
-        step +=9;
+        step += 9;
     }
 
     step = 1;
-    while (index - step >= 9*row ){
+    while (index - step >= 9 * row) {
         cells[index - step].classList.add('hover');
-        step +=1;
+        step += 1;
     }
 
     step = 1;
-    while (index + step < 9*row + 9 ){
+    while (index + step < 9 * row + 9) {
         cells[index + step].classList.add('hover');
-        step +=1;
+        step += 1;
     }
 }
 
@@ -183,35 +183,35 @@ const checkErr = (value) => {
     let box_start_row = row - row % 3;
     let box_start_col = col - col % 3;
 
-    for (let i = 0; i<CONSTANT.BOX_SIZE; i++){
-        for (let j = 0; j< CONSTANT.BOX_SIZE; j++){
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
             let cell = cells[9 * (box_start_row + i) + (box_start_col + j)];
-            if(!cell.classList.contains('selected')) addErr(cell);
+            if (!cell.classList.contains('selected')) addErr(cell);
         }
     }
 
     let step = 9;
-    while (index - step >= 0 ){
+    while (index - step >= 0) {
         addErr(cells[index - step]);
-        step +=9;
+        step += 9;
     }
 
     step = 9;
-    while (index + step < 81 ){
+    while (index + step < 81) {
         addErr(cells[index + step]);
-        step +=9;
+        step += 9;
     }
 
     step = 1;
-    while (index - step >= 9*row ){
+    while (index - step >= 9 * row) {
         addErr(cells[index - step]);
-        step +=1;
+        step += 1;
     }
 
     step = 1;
-    while (index + step < 9*row + 9 ){
+    while (index + step < 9 * row + 9) {
         addErr(cells[index + step]);
-        step +=1;
+        step += 1;
     }
 }
 
@@ -224,7 +224,7 @@ const saveGameInfo = () => {
         su: {
             original: su.original,
             question: su.question,
-            answer: su.answer
+            answer: su_answer
         }
     }
     localStorage.setItem('game', JSON.stringify(game));
@@ -244,29 +244,29 @@ const showResult = () => {
 }
 
 const initNumberInputEvent = () => {
-    number_inputs.forEach((e , index) => {
+    number_inputs.forEach((e, index) => {
         e.addEventListener('click', () => {
-            if(!cells[selected_cell].classList.contains('filled')) {
+            if (!cells[selected_cell].classList.contains('filled')) {
                 cells[selected_cell].innerHTML = index + 1;
                 cells[selected_cell].setAttribute('data-value', index + 1);
 
                 let row = Math.floor(selected_cell / CONSTANT.GRID_SIZE);
                 let col = selected_cell % CONSTANT.GRID_SIZE;
-                su_answer[row][col] = index + 1 ;
+                su_answer[row][col] = index + 1;
 
                 saveGameInfo();
                 removeErr();
                 checkErr(index + 1);
                 cells[selected_cell].classList.add('zoom-in');
                 setTimeout(() => {
-                    cells[selected_cell].classList.remove('zoom-in');  
+                    cells[selected_cell].classList.remove('zoom-in');
                 }, 500);
 
                 if (isGameWin()) {
                     removeGameInfo();
                     showResult();
                 }
-            } 
+            }
         })
     })
 }
@@ -281,37 +281,37 @@ const initCellsEvent = () => {
                 e.classList.remove('err');
                 e.classList.add('selected');
                 resetBg();
-                hoverBg(index);                
+                hoverBg(index);
             }
         })
     })
 }
 
-const startGame=()=>{
+const startGame = () => {
     start_screen.classList.remove('active');
     game_screen.classList.add('active');
 
-    player_name.innerHTML =name_input.value.trim();
+    player_name.innerHTML = name_input.value.trim();
     setPlayerName(name_input.value.trim());
 
-    game_level.innerHTML=CONSTANT.LEVEL_NAME[level_index];
-    
+    game_level.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+
     showTime(seconds);
 
 
-    timer= setInterval(() => {
-        if(!pause){
-            seconds = seconds +1;
-            game_time.innerHTML=showTime(seconds);
+    timer = setInterval(() => {
+        if (!pause) {
+            seconds = seconds + 1;
+            game_time.innerHTML = showTime(seconds);
         }
-        
+
     }, 1000);
 }
 
 const returnStartScreen = () => {
     clearInterval(timer);
-    pause=false,
-    seconds=0;
+    pause = false;
+    seconds = 0;
     start_screen.classList.add('active');
     game_screen.classList.remove('active');
     pause_screen.classList.remove('active');
@@ -357,20 +357,20 @@ document.querySelector('#btn-continue').addEventListener('click', () => {
     }
 });
 
-document.querySelector('#btn-pause').addEventListener('click',()=>{
+document.querySelector('#btn-pause').addEventListener('click', () => {
     pause_screen.classList.add('active');
-    pause=true;
+    pause = true;
 });
-document.querySelector('#btn-resume').addEventListener('click',()=>{
+document.querySelector('#btn-resume').addEventListener('click', () => {
     pause_screen.classList.remove('active');
-    pause=false;
+    pause = false;
 });
 
-document.querySelector('#btn-new-game').addEventListener('click',()=>{
+document.querySelector('#btn-new-game').addEventListener('click', () => {
     returnStartScreen();
 });
 
-document.querySelector('#btn-new-game-2').addEventListener('click',()=>{
+document.querySelector('#btn-new-game-2').addEventListener('click', () => {
     console.log('object')
     returnStartScreen();
 });
@@ -383,7 +383,7 @@ document.querySelector('#btn-delete').addEventListener('click', () => {
     let col = selected_cell % CONSTANT.GRID_SIZE;
 
     su_answer[row][col] = 0;
-    removeErr()
+    removeErr();
 })
 
 
@@ -396,15 +396,15 @@ const init = () => {
     const game = getGameInfo();
 
     document.querySelector('#btn-continue').style.display = game ? 'grid' : 'none';
-    
+
     initGameGrid();
     initCellsEvent();
     initNumberInputEvent();
-    
-    if(getPlayerName()){
-        name_input.value=getPlayerName();
+
+    if (getPlayerName()) {
+        name_input.value = getPlayerName();
     }
-    else{
+    else {
         name_input.focus();
     }
 }
--- static/js/constant.js
+++ static/js/constant.js
@@ -1,9 +1,9 @@
-const CONSTANT={
-    UNASSIGNED:0,
-    GRID_SIZE:9,
-    BOX_SIXE:3,
-    NUMBERS:[1,2,3,4,5,6,7,8,9],
-    LEVEL_NAME:[
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 9,
+    BOX_SIXE: 3,
+    NUMBERS: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    LEVEL_NAME: [
         'Easy',
         'Medium',
         'Hard',
@@ -11,5 +11,5 @@ const CONSTANT={
         'Insane',
         'Inhuman'
     ],
-    LEVEL:[29,38,47,56,65,74]
+    LEVEL: [29, 38, 47, 56, 65, 74]
 }
